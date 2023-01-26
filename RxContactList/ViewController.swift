2f000ca0c33ae92aee07067e21f548055192e8ec
Shibaeva Svetlana <68277146+svetlanashibaeva@users.noreply.github.com>
2023-01-26 12:26:06 +0500

    Initial Commit

---
+++ RxContactList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RxContactList
+//
+//  Created by Света Шибаева on 26.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
